3d31094878216a583e16318d82811eca920d5824
ViniciusItalo <vinicius.italo@kultivi.com>
2022-05-12 17:54:03 -0300

    refactore: adjusting folders and files

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Fragment } from "react";
+
+const Home: NextPage = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>Fresh Fruit | Desenvolvido por Vinicius Italo</title>
+      </Head>
+    </Fragment>
+  );
+};
+
+export default Home;
