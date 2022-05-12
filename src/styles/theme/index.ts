a13fa33fe1e588f3427faba012a61c787779ae09
ViniciusItalo <vinicius.italo@kultivi.com>
2022-05-12 18:26:49 -0300

    feat: theme config

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,29 @@
+export const theme = {
+  COLORS: {
+    ORANGE_600: "#F23005",
+    ORANGE_400: "#F28705",
+    YELLOW_300: "#F2B705",
+    GREEN_300: "#ADBF26",
+
+    WHITE: "#FFFFFF",
+    SHAPE: "#F2F2F2",
+    DARK: "#0D0D0D",
+  },
+
+  FONTS: {
+    FAMILY: {
+      DEFAULT: "'Nunito Sans', sans-serif",
+    },
+    SIZES: {
+      SMALL: "0.8rem",
+      REGULAR: "1rem",
+      MEDIUM: "1.5rem",
+      LARGE: "2rem",
+    },
+    WEIGHT: {
+      REGULAR: 400,
+      BOLD: 700,
+      BLACK: 900,
+    },
+  },
+};
